ff8538dac5c33dea745df7cf1c51cca618ccb033
Yan Bueno <buenoy519@gmail.com>
2024-09-04 12:49:58 -0300

    Adição de script pra capturar o nome do usuário e exibí-lo como mensagem de saudação na Hero Section

--- js/index.js
+++ js/index.js
@@ -9,3 +9,15 @@ function troll() {
         seeMoreTroll.textContent = "See More"
     }
 }
+
+function showName() {
+    let usernameInfo = prompt("Welcome! What's your name?")
+    let username = document.getElementById('username')
+    username.textContent = `, ${usernameInfo}!`;
+
+    if (usernameInfo === null || usernameInfo === "" || !isNaN(usernameInfo)) {
+        username.textContent = ", Stranger!";
+    } else {
+        username.textContent = `, ${usernameInfo}!`;
+    }
+} showName();
