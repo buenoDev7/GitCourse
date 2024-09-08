69cb0fa3d714e6f75f191498388f7003da828114
Yan Bueno <buenoy519@gmail.com>
2024-09-07 22:39:36 -0300

    Modificação do estilo do <button> e adição de +1 carrosel exemplo antes do <footer>

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
-function troll() {
-    const msgTroll = document.getElementById("msg-troll");
-    const seeMoreTroll = document.getElementById("seeMoreTroll")
-    msgTroll.classList.toggle("d-none")
+// function troll() {
+//     const msgTroll = document.getElementById("msg-troll");
+//     const seeMoreTroll = document.getElementById("seeMoreTroll")
+//     msgTroll.classList.toggle("d-none")
 
-    if (!msgTroll.classList.contains('d-none')) {
-        seeMoreTroll.textContent = "Hide"
-    } else {
-        seeMoreTroll.textContent = "See More"
-    }
-}
+//     if (!msgTroll.classList.contains('d-none')) {
+//         seeMoreTroll.textContent = "Hide"
+//     } else {
+//         seeMoreTroll.textContent = "See More"
+//     }
+// }
 
-function showName() {
-    let usernameInfo = prompt("Welcome! What's your name?")
-    let username = document.getElementById('username')
-    username.textContent = `, ${usernameInfo}!`;
+// function showName() {
+//     let usernameInfo = prompt("Welcome! What's your name?")
+//     let username = document.getElementById('username')
+//     username.textContent = `, ${usernameInfo}!`;
 
-    if (usernameInfo === null || usernameInfo === "" || !isNaN(usernameInfo)) {
-        username.textContent = ", Stranger!";
-    } else {
-        username.textContent = `, ${usernameInfo}!`;
-    }
-} showName();
+//     if (usernameInfo === null || usernameInfo === "" || !isNaN(usernameInfo)) {
+//         username.textContent = ", Stranger!";
+//     } else {
+//         username.textContent = `, ${usernameInfo}!`;
+//     }
+// } showName();
