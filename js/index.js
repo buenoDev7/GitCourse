6ed95bdb72a21fe32e000e90a1757c83d05581e6
Yan Bueno <buenoy519@gmail.com>
2024-10-02 00:14:54 -0300

    Reativação da função de saudação

--- js/index.js
+++ js/index.js
@@ -1,39 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.card');
+function troll() {
+    const msgTroll = document.getElementById("msg-troll");
+    const seeMoreTroll = document.getElementById("seeMoreTroll")
+    msgTroll.classList.toggle("d-none")
 
-    const observerOptions = {
-        root: null, // Observa a visibilidade dentro da viewport
-        threshold: 0.7 // Define que 50% do card precisa estar visível para acionar o callback
-    };
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                // Quando o card estiver visível, remove o blur e aplica a transformação
-                entry.target.classList.add('in-focus');
-            } else {
-                // Quando o card sair da tela, volta o blur
-                entry.target.classList.remove('in-focus');
-            }
-        });
-    }, observerOptions);
-
-    // Observa todos os cards
-    cards.forEach(card => {
-        observer.observe(card);
-    });
-});
-
-
-function noFunc() {
-    const msgNoFunc = document.getElementById("msgNoFunc");
-    const seeMoreBtn = document.getElementById("seeMoreBtn")
-    msgNoFunc.classList.toggle("d-none")
-
-    if (!msgNoFunc.classList.contains('d-none')) {
-        seeMoreBtn.textContent = "Hide"
+    if (!msgTroll.classList.contains('d-none')) {
+        seeMoreTroll.textContent = "Hide"
     } else {
-        seeMoreBtn.textContent = "See More"
+        seeMoreTroll.textContent = "See More"
     }
 }
 
@@ -47,4 +20,4 @@ function showName() {
     } else {
         username.textContent = `, ${usernameInfo}!`;
     }
-}
+} showName();
