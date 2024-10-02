627559a1198ae6b506540e79e7222380f70fba89
Yan Bueno <buenoy519@gmail.com>
2024-10-02 00:19:51 -0300

    Adição de função.js de estilos nos cards de acordo com VW do user

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.card');
+
+    const observerOptions = {
+        root: null, // Observa a visibilidade dentro da viewport
+        threshold: 0.7 // Define que 50% do card precisa estar visível para acionar o callback
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // Quando o card estiver visível, remove o blur e aplica a transformação
+                entry.target.classList.add('in-focus');
+            } else {
+                // Quando o card sair da tela, volta o blur
+                entry.target.classList.remove('in-focus');
+            }
+        });
+    }, observerOptions);
+
+    // Observa todos os cards
+    cards.forEach(card => {
+        observer.observe(card);
+    });
+});
+
+
 function troll() {
     const msgTroll = document.getElementById("msg-troll");
     const seeMoreTroll = document.getElementById("seeMoreTroll")
